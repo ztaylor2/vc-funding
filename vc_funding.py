5c43cca49b8cacf0c6527fd1991e0d0d994aece0
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-18 22:39:19 -0800

    deleted test file, created procfile for heroku.

--- vc_funding.py
+++ vc_funding.py
@@ -4,44 +4,13 @@
 import os
 import tweepy
 import time
-import sys
 import requests
 from bs4 import BeautifulSoup
-import csv
-
-# if __name__ == '__main__':
-
-def parse_page(page):
-    """Parse funding info from webpage."""
-    soup = BeautifulSoup(page.content, 'html.parser')
-    companies = soup.find_all("a", {"class": "titleLink"})
-    for company in companies:
-        print(company.text)
 
 
-    # job_summaries = []
-    # for job in jobs:
-    #     job_summaries.append(job.parent.parent.parent)
-
-    # with open('jobs.csv', 'a') as csv_file:
-    #     writer = csv.writer(csv_file)
-    #     for job in job_summaries:
-    #         title = job.find(class_='job-link').text
-    #         company = job.find(class_='-name').text
-    #         location = job.find(class_='-location').text
-    #         date_posted = job.find(class_='-posted-date').text
-    #         link = 'https://stackoverflow.com{}'.format(job.find(class_='job-link')['href'])
-    #         writer.writerow([title, company, location, date_posted, link])
-
+HEADLINES = set()
 
 page = requests.get("http://www.vcnewsdaily.com/")
-parse_page(page)
-
-
-
-
-
-# test_file = page
 
 CONSUMER_KEY = os.environ.get('CONSUMER_KEY', '')
 CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET', '')
@@ -52,10 +21,13 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-# filename = open(test_file, 'r')
-# f = filename.readlines()
-# filename.close()
+while True:
+    soup = BeautifulSoup(page.content, 'html.parser')
+    headline_a_tags = soup.find_all("a", {"class": "titleLink"})
+    for headline_a_tag in headline_a_tags:
+        if headline_a_tag.text not in HEADLINES:
+            HEADLINES.add(headline_a_tag.text)
+            api.update_status(headline_a_tag.text)
+            time.sleep(600)
 
-for line in f:
-    api.update_status(line)
-    time.sleep(900)
+    time.sleep(1800)
