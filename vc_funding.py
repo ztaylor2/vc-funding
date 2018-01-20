bdb43e10773c5299e567821ae2b8d954f55104ac
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-19 16:11:30 -0800

    added if name == main.

--- vc_funding.py
+++ vc_funding.py
@@ -9,30 +9,31 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 # satisfy heroku
 
-sched = BlockingScheduler()
+if __name__ == '__main__':
 
-HEADLINES = set()
-page = requests.get("http://www.vcnewsdaily.com/")
+    sched = BlockingScheduler()
 
-# handle twitter auth
-CONSUMER_KEY = os.environ.get('CONSUMER_KEY', '')
-CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET', '')
-ACCESS_KEY = os.environ.get('ACCESS_KEY', '')
-ACCESS_SECRET = os.environ.get('ACCESS_SECRET', '')
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+    HEADLINES = set()
+    page = requests.get("http://www.vcnewsdaily.com/")
 
+    # handle twitter auth
+    CONSUMER_KEY = os.environ.get('CONSUMER_KEY', '')
+    CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET', '')
+    ACCESS_KEY = os.environ.get('ACCESS_KEY', '')
+    ACCESS_SECRET = os.environ.get('ACCESS_SECRET', '')
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
 
-@sched.scheduled_job('interval', minutes=30)
-def twitter_bot():
-    """Scrape and tweet every 30 mins."""
-    soup = BeautifulSoup(page.content, 'html.parser')
-    headline_a_tags = soup.find_all("a", {"class": "titleLink"})
-    for headline_a_tag in headline_a_tags:
-        if headline_a_tag.text not in HEADLINES:
-            HEADLINES.add(headline_a_tag.text)
-            api.update_status(headline_a_tag.text)
-            time.sleep(600)
+    @sched.scheduled_job('interval', minutes=30)
+    def twitter_bot():
+        """Scrape and tweet every 30 mins."""
+        soup = BeautifulSoup(page.content, 'html.parser')
+        headline_a_tags = soup.find_all("a", {"class": "titleLink"})
+        for headline_a_tag in headline_a_tags:
+            if headline_a_tag.text not in HEADLINES:
+                HEADLINES.add(headline_a_tag.text)
+                api.update_status(headline_a_tag.text)
+                time.sleep(600)
 
-sched.start()
+    sched.start()
