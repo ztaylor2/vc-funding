339f6fc4d5555184b6b85939ad1a5cecc384c505
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-01-21 20:59:52 -0800

    stopping bot. bot is retweeting the same things.

--- vc_funding.py
+++ vc_funding.py
@@ -21,16 +21,17 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+# need to figure out better solution.  
 
-@sched.scheduled_job('interval', minutes=30)
-def twitter_bot():
-    """Scrape and tweet every 30 mins."""
-    soup = BeautifulSoup(page.content, 'html.parser')
-    headline_a_tags = soup.find_all("a", {"class": "titleLink"})
-    for headline_a_tag in headline_a_tags:
-        if headline_a_tag.text not in HEADLINES:
-            HEADLINES.add(headline_a_tag.text)
-            api.update_status(headline_a_tag.text)
-            time.sleep(600)
+# @sched.scheduled_job('interval', minutes=30)
+# def twitter_bot():
+#     """Scrape and tweet every 30 mins."""
+#     soup = BeautifulSoup(page.content, 'html.parser')
+#     headline_a_tags = soup.find_all("a", {"class": "titleLink"})
+#     for headline_a_tag in headline_a_tags:
+#         if headline_a_tag.text not in HEADLINES:
+#             HEADLINES.add(headline_a_tag.text)
+#             api.update_status(headline_a_tag.text)
+#             time.sleep(600)
 
-sched.start()
+# sched.start()
